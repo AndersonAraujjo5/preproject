d6ff078fc9d25f0b92be1b21cc86c0ffbae87828
Anderson Tailon <andersonaraujjo5@gmail.com>
2023-08-01 22:41:27 -0300

    t1

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -7,7 +7,7 @@ class ApiController{
     console.log('body',req.body);
     const {entry} = req.body;
     console.log('\n\n',entry[0].changes,'\n\n');
-    console.log('\n\n',JSON.parse(entry),'\n\n')
+    console.log('\n\n',JSON.stringify(entry),'\n\n')
     console.log('params',req.params);
 
     res.send(dados);
